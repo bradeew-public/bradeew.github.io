57d88c744c7ac2e7d2e534b18e0ba22aec5705dc
Bill Bradee <bradeew@gmail.com>
2014-11-10 21:35:50 -0600

    Added timeline from http://visjs.org/

--- src/setFields.js
+++ src/setFields.js
@@ -26,3 +26,70 @@ function setPtInfo() {
         "<a href='tel:" + di.tel + "'>" + di.tel + "</a>");
 }
 
+function setEncounterDetails(index, encounters){
+    enc = encounters[index]
+    $("#encounterDetails").html(
+        "Date: "+ enc["Visit Date"] + "<br/>" +
+        "Chief Complaint: " + enc["Chief Complaint"] + "<br/>" +
+        "Diagnosis: " + (enc["Diagnosis"] =="" ? "- none -" : enc["Diagnosis"]) + "<br/>" +
+        "Notes: " + enc["Notes"] + "<br/>");
+
+    /*
+    <th>Chief Complaint</th>
+    <th>Diagnosis</th>
+    <th>ICD-9</th>
+    <th>Height</th>
+    <th>Weight</th>
+    <th>Pulse</th>
+    <th>Respiration</th>
+    <th>Blood Pressure</th>
+    <th>Notes</th>
+        "Got index " + index);
+*/
+
+}
+
+function setupTimeline(encounters, patientInfo){
+    var container = document.getElementById('timeline');
+
+    // Create a DataSet (allows two way data-binding)
+    var encs = [];
+    var bd = patientInfo.birthDate;
+    bd = bd.substr(6,4) + "-" + bd.substr(0,2) + "-" + bd.substr(3,2);
+
+    /*
+     encs.push({
+     id:0,
+     content : "Birthdate",
+     start : bd,
+     end : bd
+     });
+     */
+    for (var i = 0; i<encounters.length; i++){
+        var vd = encounters[i]["Visit Date"].text;
+        var visitDate = vd.substr(0,4) + '-' + vd.substr(4,2) + '-' + vd.substr(6,2);
+        var enc = {
+            id : i,
+            content: encounters[i]["Chief Complaint"] +
+            (encounters[i]["Diagnosis"] == "" ? "" : "<br/>("+encounters[i]["Diagnosis"]+")"),
+            start : visitDate,
+            end : visitDate
+        };
+        encs.push(enc);
+    }
+
+    var items = new vis.DataSet(encs);
+
+    // Configuration for the Timeline
+    var options = {
+        max : new Date(),
+        min : new Date(patientInfo.birthDate),
+        type : 'point'
+    };
+
+    // Create a Timeline
+    var timeline = new vis.Timeline(container, items, options);
+    timeline.on('select', function (properties) {
+        setEncounterDetails(properties.items[0], encounters);
+    });
+}
